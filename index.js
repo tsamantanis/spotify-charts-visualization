108bd5a35547a9473ea39e479cc55ff61740aa72
tsamantanis <philip.tsamantanis@icloud.com>
2021-12-05 03:46:46 -0800

    Display average for each category

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import RadarChart from './radarChart.js'
+import { colors } from './constants.js';
 
 async function handleData() {
   var margin = {top: 100, right: 100, bottom: 100, left: 100},
@@ -5,33 +7,33 @@ async function handleData() {
     height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
             
   var color = d3.scaleOrdinal()
-    .range(["#7400b8", "#6930c3", "#5e60ce", "#5390d9", "#4ea8de", "#48bfe3", "#56cfe1", "#64dfdf", "#72efdd", "#80ffdb"]);
+    .range(colors);
     
   var radarChartOptions = {
     w: width,
     h: height,
     margin: margin,
     maxValue: 0.5,
-    levels: 6,
+    levels: 4,
     roundStrokes: true,
     color: color
   };
 
   function formatValue(value) {
-    return Math.abs(parseFloat(value)) > 1 ? 1 : Math.abs(parseFloat(value))
+    return Math.abs(parseFloat(value)) > 1 ? 1 : Math.abs(parseFloat(value)) || 0
   }
 
   const data = await d3.csv("./spotify_dataset.csv")
-  const formattedData = data.map(d => d["Danceability"] ?
-    [
-      { axis: "Danceability", value: formatValue(d["Danceability"]) || 0 }, 
-      { axis: "Energy", value: formatValue(d["Energy"]) || 0 }, 
-      { axis: "Loudness", value: formatValue(d["Loudness"]) || 0 }, 
-      { axis: "Speechiness", value: formatValue(d["Speechiness"]) || 0 }, 
-      { axis: "Acousticness", value: formatValue(d["Acousticness"]) || 0 }, 
-      { axis: "Liveness", value: formatValue(d["Liveness"]) || 0 }
-    ] : null
-  ).filter(d => d !== null)
+  // const formattedData = data.map(d => d["Danceability"] ?
+  //   [
+  //     { axis: "Danceability", value: formatValue(d["Danceability"]) || 0 }, 
+  //     { axis: "Energy", value: formatValue(d["Energy"]) || 0 }, 
+  //     { axis: "Loudness", value: formatValue(d["Loudness"]) || 0 }, 
+  //     { axis: "Speechiness", value: formatValue(d["Speechiness"]) || 0 }, 
+  //     { axis: "Acousticness", value: formatValue(d["Acousticness"]) || 0 }, 
+  //     { axis: "Liveness", value: formatValue(d["Liveness"]) || 0 }
+  //   ] : null
+  // ).filter(d => d !== null)
 
   // const maxDanceability = Math.max(...formattedData.map(d => d[0].value))
   // const maxEnergy = Math.max(...formattedData.map(d => d[1].value))
@@ -40,8 +42,22 @@ async function handleData() {
   // const maxAcousticness = Math.max(...formattedData.map(d => d[4].value))
   // const maxLiveness = Math.max(...formattedData.map(d => d[5].value))
   
-  // const test = formattedData.slice(0, 3)
-  // console.log(test)
+  const averageDanceability = data.reduce((acc, cur) => acc + formatValue(cur["Danceability"]), 0) / data.length
+  const averageEnergy = data.reduce((acc, cur) => acc + formatValue(cur["Energy"]), 0) / data.length
+  const averageLoudness = data.reduce((acc, cur) => acc + formatValue(cur["Loudness"]), 0) / data.length
+  const averageSpeechiness = data.reduce((acc, cur) => acc + formatValue(cur["Speechiness"]), 0) / data.length
+  const averageAcousticness = data.reduce((acc, cur) => acc + formatValue(cur["Acousticness"]), 0) / data.length
+  const averageLiveness = data.reduce((acc, cur) => acc + formatValue(cur["Liveness"]), 0) / data.length
+  
+  const formattedData = [
+    [{ axis: "Danceability", value: averageDanceability },
+    { axis: "Energy", value: averageEnergy },
+    { axis: "Loudness", value: averageLoudness },
+    { axis: "Speechiness", value: averageSpeechiness },
+    { axis: "Acousticness", value: averageAcousticness },
+    { axis: "Liveness", value: averageLiveness }]
+  ]
+
   //Call function to draw the Radar chart
   RadarChart(".radarChart", formattedData, radarChartOptions);
 
